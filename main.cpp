24af84b027184adb47caeeca7c8269b49e9abd8b
Danny Diaz <danny.jesus.diaz.94@gmail.com>
2021-02-24 21:19:32 -0600

    Added unknown args capability

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 using std::cout;
 using std::endl;
 
+// global variables from getopt.h:
+// extern char *optarg;
+// extern int optopt, optind, opterr;
+
 
 int main(int argc, char* argv[]){
 
@@ -14,12 +18,12 @@ int main(int argc, char* argv[]){
 
     cout << endl << "===============================================" << endl << endl;
 
-    int opt = getopt(argc, argv, "a:b:X");
+    int opt = getopt(argc, argv, ":a:b:X");
 
     while (opt != -1){
         switch(opt){
             case 'a': 
-                cout << "Option a passed with argument: " << optarg << endl; // optarg is a global variable declared in getopt.h
+                cout << "Option a passed with argument: " << optarg << endl;
                 break;
             case 'b': 
                 cout << "Option b passed with argument: " << optarg << endl;
@@ -27,7 +31,13 @@ int main(int argc, char* argv[]){
             case 'X': 
                 cout << "Option X passed" << endl;
                 break;
+            case '?':
+                cout << "Unknown option passed in: " << static_cast<char>(optopt) << endl;
+                break;
+            case ':':
+                cout << "Option: " << static_cast<char>(optopt) << "missing required argument" << endl;
+                break;
         }
-        opt = getopt(argc, argv, "a:b:X");
+        opt = getopt(argc, argv, ":a:b:X");
     }
 }
