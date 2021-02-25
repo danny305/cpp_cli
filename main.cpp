d34b906a67d1dde73c1fc35dae9f36ad3c51b499
Danny Diaz <danny.jesus.diaz.94@gmail.com>
2021-02-24 20:38:02 -0600

    simple getopt cli

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <getopt.h>
+
+using std::cout;
+using std::endl;
 
 
 int main(int argc, char* argv[]){
 
+
     for(int i=0; i<argc; ++i){
-        std::cout << "Arg: " << i << " = " << argv[i] << std::endl;
+        cout << "Arg: " << i << " = " << argv[i] << endl;
     }
 
-    
+    cout << endl << "===============================================" << endl << endl;
+
+    int opt = getopt(argc, argv, "abX");
+
+    while (opt != -1){
+        switch(opt){
+            case 'a': 
+                cout << "Option a passed" << endl;
+                break;
+            case 'b': 
+                cout << "Option b passed" << endl;
+                break;
+            case 'X': 
+                cout << "Option X passed" << endl;
+                break;
+        }
+        opt = getopt(argc, argv, "abX");
+    }
 }
